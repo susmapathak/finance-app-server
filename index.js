91920aa75a76465f6928f7c01fd9479d8eb60bd6
Susma Pathak <suspathak21@gmail.com>
2024-09-05 10:41:18 +1000

    mongodb connection added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var app = express();
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// DB Connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
 app.get('/', function (req, res) {
   res.send('Hello World');
-})
+});
 
 var server = app.listen(5000, function () {
   console.log("Server running on port 5000");
